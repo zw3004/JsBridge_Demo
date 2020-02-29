3f4c0099fac12edf7acdcb9319f0b55621e458a1
周伟 <zhouwei_26@xueda.com>
2020-02-29 18:12:23 +0800

    demo ok

--- app/src/main/java/com/android/jsbridgedemo/LauchActivity.java
+++ app/src/main/java/com/android/jsbridgedemo/LauchActivity.java
@@ -13,12 +13,17 @@ public class LauchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_lauch);
+        //判断不是第一次进入则直接关闭
+        if ((getIntent().getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0) {
+            finish();
+            return;
+        }
+
+        //进入真正的主Activity关闭掉自己
         Intent intent = new Intent();
-        intent.setClass(LauchActivity.this,StartActivity.class);
         //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         //intent.setAction("com.android.jsbridgedemo.MainActivity");
-
-
+        intent.setClass(LauchActivity.this,StartActivity.class);
         startActivity(intent);
         (new Handler()).postDelayed(new Runnable() {
             public void run() {
